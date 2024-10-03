c44590eeec018c306bf2ee4a60bec33f4c4cabff
a11dev <alessandro1leonardi@gmail.com>
2024-10-03 10:37:33 +0200

    added data on broadcast stop receiver

--- app/src/main/java/dev/a11dev/trackapp/StartTrackingReceiver.java
+++ app/src/main/java/dev/a11dev/trackapp/StartTrackingReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.net.Uri;
 
 public class StartTrackingReceiver extends BroadcastReceiver {
 
@@ -14,7 +15,18 @@ public class StartTrackingReceiver extends BroadcastReceiver {
         if (intent != null && TrackingService.ACTION_START_TRACKING.equals(intent.getAction())) {
             Log.d(TAG, "Start intent received in static receiver");
             // Avvia la logica di stop del TrackingService
+            String action = intent.getAction();
+            Uri dataUri = intent.getData();
 
+            if ("dev.a11dev.trackapp.ACTION_STOP_TRACKING".equals(action) && dataUri != null) {
+                Log.d(TAG, "Received Stop Tracking Broadcast with URI: " + dataUri.toString());
+
+                // Handle the URI accordingly
+                // For example, stop tracking and log the received URI
+                // Custom handling code here...
+            } else {
+                Log.d(TAG, "Stop Tracking Broadcast received without a URI");
+            }
             Intent serviceIntent = new Intent(context, TrackingService.class);
             context.startService(serviceIntent);
 
